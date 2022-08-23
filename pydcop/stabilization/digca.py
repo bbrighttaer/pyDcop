b8edf120d9da8ee3a68737b61b3435d1d1fcce14
Brighter <brighteragyemang@gmail.com>
2022-08-24 04:30:38 +1000

    added broadcast msg support to discovery comp

--- pydcop/stabilization/digca.py
+++ pydcop/stabilization/digca.py
@@ -1,6 +1,19 @@
 from pydcop.infrastructure.agents import DynamicAgent
-from pydcop.infrastructure.computations import MessagePassingComputation
-from pydcop.infrastructure.discovery import Discovery
+from pydcop.infrastructure.computations import MessagePassingComputation, Message, message_type
+from pydcop.infrastructure.discovery import Discovery, BroadcastMessage
+from pydcop.infrastructure.orchestrator import ORCHESTRATOR
+from pydcop.stabilization.protocol import DynamicGraphConstructionComputation
+
+NAME = 'DIGCA'
+
+# states
+INACTIVE = 'inactive'
+ACTIVE = 'active'
+
+AnnounceMessage = message_type(
+    'AnnounceMessage',
+    fields=['agent_id', 'address']
+)
 
 
 def build_stabilization_computation(agent: DynamicAgent, discovery: Discovery) -> MessagePassingComputation:
@@ -21,17 +34,29 @@ def build_stabilization_computation(agent: DynamicAgent, discovery: Discovery) -
     return DIGCA(agent, discovery)
 
 
-class DIGCA(MessagePassingComputation):
-
+class DIGCA(DynamicGraphConstructionComputation):
     """
     Implements the Dynamic Interaction Graph Construction Algorithm.
     """
 
     def __init__(self, agent: DynamicAgent, discovery: Discovery):
-        super(DIGCA, self).__init__(name=f'{agent.name}-digca')
-
-        self.discovery = discovery
+        super(DIGCA, self).__init__(NAME, agent, discovery)
 
     def on_start(self):
         self.logger.debug(f'On start of {self.name}')
+        self.connect()
 
+    def on_message(self, sender: str, msg: Message, t: float):
+        self.logger.debug(f'Received message {msg} from {sender}')
+
+    def connect(self):
+        self.logger.debug('DIGCA is connecting...')
+        self.post_msg(
+            '_discovery_' + ORCHESTRATOR,
+            BroadcastMessage(message=AnnounceMessage(
+                agent_id=self.agent.name,
+                address=self.address
+            ),
+                recipient_prefix=NAME
+            )
+        )
